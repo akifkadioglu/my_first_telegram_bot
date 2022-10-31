d367ff772831a0daa027b82ee1f208e5dc7ff499
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2022-10-31 15:23:01 +0300

    added ai

--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
+
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
+	replicate "github.com/sausheong/goreplicate"
 )
 
 func main() {
+
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("error loading .env file")
 	}
-	fmt.Println(os.Getenv("TELEGRAM_API"))
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API"))
 	if err != nil {
 		fmt.Print(err)
@@ -23,14 +25,30 @@ func main() {
 	u.Timeout = 60
 	updates := bot.GetUpdatesChan(u)
 	for update := range updates {
+
 		if update.Message == nil {
 			continue
 		}
-		returnmsg := "asdasd"
-		if update.Message.Text == "naber" {
-			returnmsg = "iyi sen"
+
+		model := replicate.NewModel("stability-ai", "stable-diffusion", os.Getenv("DALL_E_MINI_VERSION"))
+
+		model.Input["prompt"] = update.Message.Text
+		model.Input["num_outputs"] = 1
+		client := replicate.NewClient(os.Getenv("DALL_E_MINI_API"), model)
+		err := client.Create()
+		if err != nil {
+			fmt.Println("bir hata")
 		}
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, returnmsg)
+		err = client.Get(client.Response.ID)
+		if err != nil {
+			fmt.Println("bir hata")
+		}
+		content := "Something went wrong, try something else!"
+
+		if len(client.Response.Output) > 0 {
+			content = client.Response.Output[0]
+		}
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, content)
 		bot.Send(msg)
 	}
 }
